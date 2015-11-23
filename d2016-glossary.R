014d702d55bba963b76a6b41f2ae6319b7ea4aed
Brooks Ambrose <brooksambrose@gmail.com>
2015-11-23 12:18:04 -0800

    removing draftin.com bridges; too complicated

---
+++ d2016-glossary.R
@@ -0,0 +1,30 @@
+nicen<-function(x) format(x,big.mark=',',big.interval=3,nsmall=3)
+
+onu<-list(
+	sf='subfield'
+	,sd='subdiscipline'
+	,ic='invisible college'
+	,fp='field position'
+	,sp='specialty'
+	,fi='field of inquiry'
+	,sc='subculture'
+) # interchangeable ontological units
+onus<-list(
+	sf='subfields'
+	,sd='subdisciplines'
+	,ic='invisible colleges'
+	,fp='field positions'
+	,sp='specialties'
+	,fi='fields of inquiry'
+	,sc='subcultures'
+) # interchangeable ontological units
+
+opu<-list(
+	kcc='k-clique community'
+	,kca='kcc'
+) # interchangeable operational units
+opus<-list(
+	kcc='k-clique communities'
+	 ,kca='kccs'
+) # interchangeable operational units
+
