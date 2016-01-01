1b827c4dc0e009952276a2ce01bc3d1ddbad7be4
Brooks Ambrose <brooksambrose@gmail.com>
2016-01-01 10:30:26 -0800

    evidentiary foundations
    ggplot2 for exploring patterns of missing data

--- d2016-glossary.R
+++ d2016-glossary.R
@@ -1,5 +1,8 @@
 nicen<-function(x) format(x,big.mark=',',big.interval=3,nsmall=3)
 
+#WOK Field Tags, https://images.webofknowledge.com/WOKRS58B4/help/WOK/hs_wos_fieldtags.html
+wokfld<-data.table(read.table('resources/wokfields.txt',sep = '\t',header = TRUE),key='field')
+
 onu<-list(
 	sf='subfield'
 	,sd='subdiscipline'
